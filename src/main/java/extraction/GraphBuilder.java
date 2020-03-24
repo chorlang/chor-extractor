fc5c03112b80854bc2333da3e4d5cd7682fc2941
Bjørn <bjoernak@gmail.com>
2020-03-24 18:35:27 +0100

    Added JavaDoc to GraphBuilder.java

--- src/main/java/extraction/GraphBuilder.java
+++ src/main/java/extraction/GraphBuilder.java
@@ -6,6 +6,11 @@ import org.jgrapht.graph.DirectedPseudograph;
 
 import java.util.*;
 
+/**
+ * Class for extracting a graph from a Network using a specific extraction strategy.
+ * The graph symbolises the networks states throughout execution, and the communication between the network's processes.
+ * The graph is intended to be used for choreography extraction.
+ */
 public class GraphBuilder {
     private Strategy strategy;
     private GraphExpander expander;
@@ -14,10 +19,20 @@ public class GraphBuilder {
         OK, BADLOOP, FAIL
     }
 
+    /**
+     * Instantiates a new GraphBuilder
+     * @param extractionStrategy The Strategy used to extract a graph from the Network with networkGraphBuilder().
+     */
     public GraphBuilder(Strategy extractionStrategy){
         strategy = extractionStrategy;
     }
 
+    /**
+     * Constructs a graph symbolizing the execution of the Network AST parsed to this method.
+     * @param n The network to construct a graph from.
+     * @param services A list of processes that are allowed to be livelocked.
+     * @return A directed graph where each vertex symbolizes a state of the network, and the edges the interactions that change from the state in one vertex to the other.
+     */
     public DirectedPseudograph<Node, Label> networkGraphBuilder(Network n, Set<String> services){
         var marking = new HashMap<String, Boolean>();
         n.processes.forEach((processName, processTerm) -> marking.put(processName, processTerm.main.getAction() == Behaviour.Action.termination || services.contains(processName)));
@@ -30,6 +45,12 @@ public class GraphBuilder {
         return expander.getGraph();
     }
 
+    /**
+     * Builds the graph recursively from the node parsed to this method.
+     * This method is not intended to be called externally. Use networkGraphBuilder() instead.
+     * @param currentNode The node to build a branch of the graph from.
+     * @return OK if successful, BADLOOP if adding the node would create a loop where not all processes are marked (used), or FAIL if the graph could not be build, making choreography extraction impossible.
+     */
     BuildGraphResult buildGraph(ConcreteNode currentNode){
         HashSet<String> unfoldedProcesses = new HashSet<>();
         LinkedHashMap<String, ProcessTerm> processes = copyAndSortProcesses(currentNode);
@@ -97,6 +118,13 @@ public class GraphBuilder {
         }
     }
 
+    /**
+     * If the ProcessTerm's main action is conditional, returns labels and networks resulting from the oconditional action, both for the then case, and else case.
+     * @param processes The map of ProcessTerms the graph is being build from
+     * @param processName The name of the process currently being prospected for conditional action.
+     * @param processTerm The ProcessTerm currently being prospected for conditional action.
+     * @return A ConditionContainer with labels and Networks resulting from the then case, and else case.
+     */
     private ConditionContainer findConditional(HashMap<String, ProcessTerm> processes, String processName, ProcessTerm processTerm){
         if (processTerm.main.getAction() != Behaviour.Action.condition)
             return null;
@@ -124,6 +152,14 @@ public class GraphBuilder {
         return true;
     }
 
+    /**
+     * If the ProcessTerm's main action is information exchange between two processes, returns a label for such action,
+     * and the Network that would result from that action.
+     * @param processes The map of ProcessTerms the graph is being build from.
+     * @param processName The name of the process currently being prospected for communication.
+     * @param processTerm The processTerm for the process currently being prospected for communication
+     * @return A CommunicationContainer with the Label and Network resulting from executing the next action in the ProcessTerm, or null if that action is not related to communication.
+     */
     private CommunicationContainer findCommunication(HashMap<String, ProcessTerm> processes, String processName, ProcessTerm processTerm){
         Behaviour main = processTerm.main;
         switch (main.getAction()){
@@ -208,6 +244,11 @@ public class GraphBuilder {
         }
     }
 
+    /**
+     * If the ProcessTerm's main Behaviour is ProcedureInvocation, the main Behaviour is replaced by its procedure definition recursively, until it is no longer a ProcedureInvocation.
+     * @param processTerm The term to unfold.
+     * @return true if the ProcessTerm was unfolded, false if the ProcessTerm is not ProcedureInvocation.
+     */
     private boolean unfold(ProcessTerm processTerm){
         Behaviour main = processTerm.main;
 
@@ -217,10 +258,9 @@ public class GraphBuilder {
         ProcedureInvocation mainProcedure = (ProcedureInvocation)main;
         String procedureName = mainProcedure.procedure;
         Behaviour procedureBehaviour = processTerm.procedures.get(procedureName);
-        if (procedureBehaviour == null){
-            System.out.println("Cannot unfold the process. Procedure definition do not exists");
-            System.exit(1);
-        }
+        if (procedureBehaviour == null)
+            throw new IllegalStateException("Cannot unfold the process. Procedure definition for " + procedureName + " do not exists");
+
         processTerm.main = procedureBehaviour.copy();
 
         if (procedureBehaviour.getAction() == Behaviour.Action.procedureInvocation){
@@ -229,13 +269,26 @@ public class GraphBuilder {
         return true;
     }
 
+    /**
+     * Folds back unfolded processes.
+     * From a list of processes which have been unfolded, in the targetNetwork,
+     * replace the main Behaviours of each ProcessTerm with a copy of the main Behaviour of the ProcessTerm in the Network of the node.
+     * @param unfoldedProcesses The set of process names for which their corresponding ProcessTerm has been unfolded.
+     * @param targetNetwork The Network for which to fold back its ProcessTerms, if they are listed in unfoldedProcesses.
+     * @param node A node containing a copy of the Network from before the ProcessTerms was unfolded.
+     */
     private void fold(HashSet<String> unfoldedProcesses, Network targetNetwork, ConcreteNode node){
         for (String processName : unfoldedProcesses){
             targetNetwork.processes.get(processName).main = node.network.processes.get(processName).main.copy();
         }
-        //unfoldedProcesses.forEach(process -> targetNetwork.processes.get(process).main = node.network.processes.get(process).main.copy());
     }
 
+    /**
+     * Returns a ordered HashMap of the processes of the Network in the node, sorted after the strategy this GraphBuilder uses, such that higher priority ProcessTerms are earlier in the HashMap.
+     * The ProcessTerms in the HashMap are copies of the original, so changes to the elements in the original HashMap do not affect the copy and vice versa.
+     * @param node The node containing the Network with a list of ProcessTerms to be sorted
+     * @return A ordered HashMap sorted after this instances extraction strategy.
+     */
     private LinkedHashMap<String, ProcessTerm> copyAndSortProcesses(Node.ConcreteNode node){
         return strategy.copyAndSort(node.network.processes);
     }
