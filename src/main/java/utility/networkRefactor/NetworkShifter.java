2b06e4281c52312aabe59f8745a4491267e67918
Bjørn <bjoernak@gmail.com>
2020-04-19 23:57:01 +0200

    Implemented NetworkShifter and NetworkUnfolder

---
+++ src/main/java/utility/networkRefactor/NetworkShifter.java
@@ -0,0 +1,99 @@
+package utility.networkRefactor;
+
+import extraction.network.*;
+import extraction.network.utils.NetworkPurger;
+import parsing.Parser;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
+
+public class NetworkShifter {
+    private static Random random = new Random();
+
+    private static class BPair{
+        Behaviour prefix, newBody;
+        BPair(Behaviour first, Behaviour second){
+            this.prefix = first;
+            this.newBody = second;
+        }
+    }
+
+    public static Network compute(String inputNetwork, double p){
+        var n = Parser.stringToNetwork(inputNetwork);
+        NetworkPurger.purgeNetwork(n);
+
+        for (var processTerm : n.processes.values()){
+            var newCalls = new HashMap<String, Behaviour>();
+            var newDefs = new HashMap<String, Behaviour>();
+            processTerm.procedures.forEach((procedureName, procedureBody) -> {
+                var shift = computeShift(procedureName, procedureBody, p);
+                newCalls.put(procedureName, shift.prefix);
+                newDefs.put(procedureName, shift.newBody);
+            });
+            processTerm.main = replace(newCalls, processTerm.main);
+            newDefs.forEach( (name, newDef) -> processTerm.procedures.put(name, replace(newCalls, newDef)));
+        }
+        return n;
+    }
+
+    private static Behaviour replace(Map<String, Behaviour> newCalls, Behaviour b){
+        switch (b.getAction()){
+            case TERMINATION:
+                return b;
+            case PROCEDURE_INVOCATION:
+                var p = (ProcedureInvocation)b;
+                return newCalls.get(p.procedure).copy();
+            case SEND:
+                var sen = (Send)b;
+                return new Send(sen.receiver, sen.expression, replace(newCalls, sen.continuation));
+            case RECEIVE:
+                var r = (Receive)b;
+                return new Receive(r.sender, replace(newCalls, r.continuation));
+            case SELECTION:
+                var sel = (Selection)b;
+                return new Selection(sel.receiver, sel.label, replace(newCalls, sel.continuation));
+            case OFFERING:
+                var o = (Offering)b;
+                var branches = new HashMap<String, Behaviour>();
+                o.branches.forEach((label, branch) -> branches.put(label, replace(newCalls, branch)));
+                return new Offering(o.sender, branches);
+            case CONDITION:
+                var c = (Condition)b;
+                return new Condition(c.expression, replace(newCalls, c.thenBehaviour), replace(newCalls, c.elseBehaviour));
+            default:
+                throw new IllegalStateException();
+        }
+    }
+
+    private static BPair computeShift(String name, Behaviour b, double p){
+        switch (b.getAction()){
+            case SEND:
+                if (random.nextDouble() < p){
+                    var s = (Send)b;
+                    var shift = computeShift(name, s.continuation, p);
+                    return new BPair(new Send(s.receiver, s.expression, shift.prefix), shift.newBody);
+                } else{
+                    return new BPair(new ProcedureInvocation(name), b);
+                }
+            case RECEIVE:
+                if (random.nextDouble() < p){
+                    var r = (Receive)b;
+                    var shift = computeShift(name, r.continuation, p);
+                    return new BPair(new Receive(r.sender, shift.prefix), shift.newBody);
+                }else {
+                    return new BPair(new ProcedureInvocation(name), b);
+                }
+            case SELECTION:
+                if (random.nextDouble() < p){
+                    var s = (Selection)b;
+                    var shift = computeShift(name, s.continuation, p);
+                    return new BPair(new Selection(s.receiver, s.label, shift.prefix), shift.newBody);
+                } else {
+                    return new BPair(new ProcedureInvocation(name), b);
+                }
+            default:
+                return new BPair(new ProcedureInvocation(name), b);
+        }
+    }
+}
---
+++ src/main/java/utility/networkRefactor/NetworkUnfolder.java
@@ -0,0 +1,76 @@
+package utility.networkRefactor;
+
+import extraction.network.*;
+import extraction.network.utils.NetworkPurger;
+import org.jetbrains.annotations.NotNull;
+import parsing.Parser;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
+
+/*
+This file was cursed.
+It took several minutes for IntelliJ to update errors and warnings, and it often did it wrong.
+When trying to commit, I got a warning about being unable to check for null expressions in loops. (or something like that)
+
+The cursed code turned out to be
+            for (var name : result.procedures.keySet()){
+                result.procedures.put(name, compute(result.procedures.get(name), processTerm.procedures, p));
+            }
+specifically the loop body seemed to be the power behind the curse. Replacing it by replaceAll()
+seems to have fixed it. I pray it has been fixed for good
+ */
+public class NetworkUnfolder {
+    private static Random random = new Random();
+
+    private static ProcessTerm compute(ProcessTerm processTerm, double p, int iterations){
+        var result = processTerm.copy();
+        for (int i = 0; i < iterations; i++){
+            result.main = compute(result.main, result.procedures, p);
+            result.procedures.replaceAll((n, v) -> compute(result.procedures.get(n), processTerm.procedures, p));
+        }
+        return result;
+    }
+
+    public static Network compute(String inputNetwork, double p, int iterations){
+        var n = Parser.stringToNetwork(inputNetwork);
+        NetworkPurger.purgeNetwork(n);
+
+        n.processes.replaceAll((k, v) -> compute(n.processes.get(k), p, iterations));
+        return n;
+    }
+
+    private static Behaviour compute(Behaviour b, Map<String, Behaviour> procedures, double p){
+        switch (b.getAction()){
+            case TERMINATION:
+                return Termination.getTermination();
+            case SEND:
+                var s = (Send)b;
+                return new Send(s.receiver, s.expression, compute(s.continuation, procedures, p));
+            case RECEIVE:
+                var r = (Receive)b;
+                return new Receive(r.sender, compute(r.continuation, procedures, p));
+            case SELECTION:
+                var sel = (Selection)b;
+                return new Selection(sel.receiver, sel.label, compute(sel.continuation, procedures, p));
+            case OFFERING:
+                Offering o = (Offering)b;
+                var branches = new HashMap<String, Behaviour>();
+                o.branches.forEach((key, value) -> branches.put(key, compute(value, procedures, p)));
+                return new Offering(o.sender, branches);
+            case CONDITION:
+                var c = (Condition)b;
+                return new Condition(c.expression, compute(c.thenBehaviour, procedures, p), compute(c.elseBehaviour, procedures, p));
+            case PROCEDURE_INVOCATION:
+                var proc = (ProcedureInvocation)b;
+                if (random.nextDouble() < p){
+                    return procedures.get(proc.procedure).copy();
+                } else{
+                    return new ProcedureInvocation(proc.procedure);
+                }
+            default:
+                throw new IllegalStateException();
+        }
+    }
+}
