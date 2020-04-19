750ca31a25a26f91f38187b82b3a93aefd628351
Bjørn <bjoernak@gmail.com>
2020-04-19 21:48:49 +0200

    Expanded package utility with LengthOfProcedures which counts the length of all procedures in a Choreography.

---
+++ src/main/java/utility/choreographyStatistics/LengthOfProcedures.java
@@ -0,0 +1,42 @@
+package utility.choreographyStatistics;
+
+import extraction.choreography.*;
+import extraction.network.utils.TreeVisitor;
+
+import java.util.ArrayList;
+
+public class LengthOfProcedures implements TreeVisitor<Integer, ChoreographyASTNode> {
+    @Override
+    public Integer Visit(ChoreographyASTNode hostNode){
+        switch (hostNode.getType()){
+            case CONDITION:{
+                var host = (Condition)hostNode;
+                return host.thenChoreography.accept(this) + host.elseChoreography.accept(this);
+            }
+            case PROCEDURE_DEFINITION:{
+                var host = (ProcedureDefinition)hostNode;
+                return host.body.accept(this);
+            }
+
+            case COMMUNICATION:
+            case SELECTION:{
+                var host = (ChoreographyBody.Interaction)hostNode;
+                return host.continuation.accept(this) + 1;
+            }
+
+            case TERMINATION:
+            case PROCEDURE_INVOCATION:
+                return 1;
+            case CHOREOGRAPHY:
+            case PROGRAM:
+            default:
+                throw new UnsupportedOperationException();
+        }
+    }
+
+    public ArrayList<Integer> getLength(Choreography chorRoot){
+        var stat = new ArrayList<Integer>();
+        chorRoot.procedures.forEach(procedure -> stat.add(procedure.accept(this)));
+        return stat;
+    }
+}
