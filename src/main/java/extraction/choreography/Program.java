0e610909ed7d2ceda27e009ab043f0690225646f
Bjørn Kjær <bjoernak@gmail.com>
2021-10-04 01:13:38 +0200

    Fixed toString() in Program.java for empty programs.

--- src/main/java/extraction/choreography/Program.java
+++ src/main/java/extraction/choreography/Program.java
@@ -20,6 +20,8 @@ public class Program extends ChoreographyASTNode{
         var builder = new StringBuilder();
         //Here it is important to use String.valueOf(choreography) (called implicit) rather than choreography.toString() to handle null values
         choreographies.forEach(choreography -> builder.append(choreography).append(" || "));
+        if (builder.isEmpty())
+            builder.append("(Empty Program)1234");
         builder.delete(builder.length() - 4, builder.length()); //Remove last four characters to remove trailing " || ".
         return builder.toString();
     }
